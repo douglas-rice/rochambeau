2c78c74cd742dd11774df278a5eb34d97a7bdf12
Douglas Rice <rice.doug@gmail.com>
2015-06-28 20:16:21 -0700

    sync files

---
+++ rochambeau.rb
@@ -0,0 +1,57 @@
+# Game: Paper, Rock, Scissors
+
+# Flow of game:
+
+# 1. Have the player pick paper, rock, or scissors.
+# 2. Have the computer pick paper, rock, or scissors.
+# 3. Compare: paper > rock, rock > scissors, scissors > paper, or tie
+# if same.
+# 3. Play again?
+
+# constant that keeps track of the available choices
+CHOICES = {'p' => 'Paper', 'r' => 'Rock', 's' => 'Scissors'}
+puts "Welcome to the Rochambeau tournament!"
+
+def display_winning_message(winning_choice)
+  case winning_choice
+  when 'p'
+    puts "Paper covers rock."
+  when 'r'
+    puts "Rock crushes scissors."
+  when 's'
+    puts "Scissors cut through paper."
+  end
+end
+
+loop do
+  # player makes a pick
+  begin
+    puts "Pick one: (p, r, s):"
+    player_choice = gets.chomp.downcase
+  end until CHOICES.keys.include?(player_choice)
+  # computer makes a pick
+  computer_choice = CHOICES.keys.sample
+
+  # compare
+  # Tie game
+  if player_choice == computer_choice
+    puts "Tie game."
+
+  # player wins
+  elsif (player_choice == 'p' && computer_choice == 'r') ||
+        (player_choice == 'r' && computer_choice == 's') ||
+        (player_choice == 's' && computer_choice == 'p')
+    # method for displaying winning message (passing in winning choice)
+    display_winning_message(player_choice)
+    puts "You win!"
+  # computer wins  
+  else
+    display_winning_message(computer_choice)
+    puts "Computer wins."
+  end
+puts "Do you want to play again? Press 'y' or 'n' "
+  break if gets.chomp.downcase != 'y' # anything user types that does 
+                                      # not equal yes will cause break
+end
+
+puts "Thanks for playing in the Rochambeau tournament!"
